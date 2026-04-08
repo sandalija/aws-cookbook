8b9918557d38814b846dcb216be8bf180198c4c4
Sergio <sergiocava.d@gmail.com>
2026-04-08 20:47:17 +0000

    ci: configure GH Pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,8 +24,9 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "sandalija", // Usually your GitHub org/user name.
+  projectName: "aws-cookbook", // Usually your repo name.
+  deploymentBranch: "main",
 
   onBrokenLinks: "throw",
 
